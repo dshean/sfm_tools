153a6b6fe76fe6d3ead8473dbca58aaabbf5456a
David Shean <dshean@gmail.com>
2013-11-04 20:45:00 -0800

    overhaul

--- exif2gpslog.sh
+++ exif2gpslog.sh
@@ -1,12 +1,18 @@
 #! /bin/bash
 
+#David Shean
+#dshean@gmail.com
+#11/4/13
+
 #NOTE: Nikon GP-1 GPS altitude is MSL, not WGS84 ellipsoid
 #Nikon GP-1 accuracy is 10 m RMS (as stated in manual), assumed to be horizontal
 #Print UTC and local time, can use $Timezone tag
 
 #See http://www.sno.phy.queensu.ca/~phil/exiftool/geotag.html
 
-dir=$(pwd)
+#dir=$(pwd)
+dir=$1
+out=$2
 
 #fmt=/Users/dshean/src/sfm/gpx.fmt
 #fmt=/Users/dshean/src/sfm/kml.fmt
@@ -22,22 +28,21 @@ dir=$(pwd)
 fmt_str='$FileName, $SubSecDateTimeOriginal, $GPSDateTime, $GPSLatitude#, $GPSLongitude#, $GPSAltitude#, $LensID, $FocalLength#, $ShutterSpeed, $Aperture, $ISO, $FOV, $FocusDistance, $DOF'
 
 #exiftool -progress -m -r -c '%.6f' -p "$fmt_str" $dir/*.JPG 
-echo $fmt_str > out.csv
-exiftool -progress -if '$GPSDateTime' -fileOrder GPSDateTime -m -r -ext JPG -p "$fmt_str" $dir >> out.csv 
+echo $fmt_str > $out.csv
+exiftool -progress -if '$GPSDateTime' -fileOrder GPSDateTime -m -r -ext JPG -p "$fmt_str" $dir >> $out.csv 
 
-#EPSG:4326
-#$GPSLatitude# $GPSAltitude#
-#csv2vrt.py out.csv
-echo -n > out.vrt
-echo '<OGRVRTDataSource>' >> out.vrt
-echo '   <OGRVRTLayer name="out">' >> out.vrt
-echo '         <SrcDataSource>out.csv</SrcDataSource>' >> out.vrt
-echo '         <GeometryType>wkbPoint25D</GeometryType>' >> out.vrt
-echo '         <LayerSRS>EPSG:4326</LayerSRS>' >> out.vrt
-echo '         <GeometryField encoding="PointFromColumns" x="$GPSLongitude#" y="$GPSLatitude#" z="$GPSAltitude#"/>' >> out.vrt
-echo '     </OGRVRTLayer>' >> out.vrt
-echo '</OGRVRTDataSource>' >> out.vrt
+#Write out vrt for csv
+echo -n > $out.vrt
+echo '<OGRVRTDataSource>' >> $out.vrt
+echo "      <OGRVRTLayer name=\"$out\">" >> $out.vrt
+echo "         <SrcDataSource>$out.csv</SrcDataSource>" >> $out.vrt
+echo '         <GeometryType>wkbPoint25D</GeometryType>' >> $out.vrt
+echo '         <LayerSRS>EPSG:4326</LayerSRS>' >> $out.vrt
+echo '         <GeometryField encoding="PointFromColumns" x="$GPSLongitude#" y="$GPSLatitude#" z="$GPSAltitude#"/>' >> $out.vrt
+echo '      </OGRVRTLayer>' >> $out.vrt
+echo '</OGRVRTDataSource>' >> $out.vrt
 
-ogr2ogr out.shp out.vrt
+#Convert to ESRI Shapefile
+ogr2ogr -overwrite $out.shp $out.vrt
 
 #ogr2ogr -f GPX -dsco GPX_USE_EXTENSIONS=YES out.gpx out.vrt
--- tpo2exif.sh
+++ tpo2exif.sh
@@ -1,15 +1,62 @@
 #! /bin/bash
 
-#Input: shp from Trimble Pathfinder Office shp
-shp_fn = $1
-photo_dir = $2
+#David Shean
+#dshean@gmail.com
+#11/4/13
+
+#Requires exiftool, gpsbabel, ogr/gdal
+
+#Input should be csv output from Trimble Pathfinder Office
+#"Configurable ascii"
+#Positions only - One point per GPS position
+#CSV template (note: NO {GPS Time}) 
+#{Feature ID} {Latitude} {Longitude} {HAE} {Attributes}
+#Check "use template as header"
+
+#NOTE: Apparently, we need to use "Date recorded" and "Time recorded" output from TPO
+#This was ~7 seconds earlier than the recorded GPS Time for each point in the 20130716 Greenland hike
+#This syncs with the Nikon GP-1 GPS timestamps
+
+in_fn=$1
+photo_dir=$2
+
+#Output original photo locations as shp
+out=exif_coord_original
+exif2shp.sh $photo_dir $out
 
 #Convert to gpx
-gpx_fn = ${shp_fn%%*.shp}.gpx
-ogr2ogr -f GPX -dsco GPX_USE_EXTENSIONS=yes $gpx_fn $shp_fn
+gpx_fn=${in_fn%%.*}.gpx
+#ogr2ogr -f GPX -dsco GPX_USE_EXTENSIONS=yes $gpx_fn $shp_fn
 
-#Sync with exif timestamp and write interpolated GPS coord to exif header
-geosync_offset = 0.0
-exiftool -Geotag $gpx_fn -Geosync=$geosync_offset "-Geotime<SubSecDateTimeOriginal" $photo_dir
+#Convert TPO to gpx track
+echo "Converting $in_fn to $gpx_fn"
+#Assuming TPO output (csv) has header with standard field names, the following will work 
+#utc=0 is required to set GPS time zone to UTC, not local time zone
+#-t creates a track instead of waypoints
+gpsbabel -t -i unicsv,utc=0 -f $in_fn -o GPX -F $gpx_fn
 
-#exiftool -exif:GPSLatitude=$xyz[2] -exif:GPSLatitudeRef=$latref -exif:GPSLongitude=$xyz[1] -exif:GPSLongitudeRef=$lonref -exif:GPSAltitude=$xyz[3] -exif:GPSAltitudeRef\#=$altref $img
+echo "Updating geotagging in $photo_dir"
+
+#Timezone offset of exif DateTimeOriginal relative to UTC
+#This is the value for Greenland 2013 images (doh!)
+#Should be 0 when camera clock is set to UTC
+tz="-02:00"
+
+#This is the offset between the camera clock and GeoXH GPS time, should only be a few seconds
+geosync_offset=0.0
+
+#Could also use this, but not all images have valid GPS info from Nikon GP-1
+#"-Geotime<GPSDateTime"
+#The exiftool doc describes a way to use certain photos as tiepoints for -Geosync
+#Should be able to script this to use all available exif GPS times
+
+#Use Original DateTime with Subsecond (precision 0.1 s for D800) time in exif header
+exiftool -progress -Geotag $gpx_fn -Geosync=$geosync_offset "-Geotime<\${SubSecDateTimeOriginal}${tz}" $photo_dir
+
+#Move original jpg to subfolder
+mkdir $photo_dir/jpg_original
+mv $photo_dir/*.jpg_original $photo_dir/jpg_original
+
+#Output corrected photo locations as shp
+out=exif_coord_corrected
+exif2shp.sh $photo_dir $out
